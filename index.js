a345d2c550d39d2214fa2eb3581e0aa98438508d
anshu1612 <anshuchakrawarti26@gmail.com>
2025-01-10 20:37:30 +0530

    wrap chevron in button

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const images = document.querySelectorAll(".image");
 const prev = document.getElementById("prevBtn");
 const next = document.getElementById("nxtBtn");
-const greyBtn = document.querySelectorAll("button");
+const greyBtn = document.querySelectorAll("button.btn");
 let i = 0;
 
 next.addEventListener("click", () => {
   images.forEach((img, index) => {
-    img.classList.add("hidden");
     greyBtn[index].classList.remove("bg-slate-500");
     greyBtn[index].classList.add("bg-slate-300");
+    img.classList.add("hidden");
   });
 
   i = (i + 1) % images.length;
-
   greyBtn[i].classList.add("bg-slate-500");
   images[i].classList.remove("hidden");
 });
 
 prev.addEventListener("click", () => {
   images.forEach((img, index) => {
-    img.classList.add("hidden");
     greyBtn[index].classList.remove("bg-slate-500");
     greyBtn[index].classList.add("bg-slate-300");
+    img.classList.add("hidden");
   });
 
   i = (i - 1 + images.length) % images.length;
-
   greyBtn[i].classList.add("bg-slate-500");
   images[i].classList.remove("hidden");
 });
-
-
